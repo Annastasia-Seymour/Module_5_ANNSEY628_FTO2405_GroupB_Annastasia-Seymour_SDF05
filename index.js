7d19d2164e8d7ec1b34617da44b2674d2903cae3
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-05-22 14:41:58 +0200

    Commit complete after learning object and method

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
+let player = {
+    name : "Per",
+    chips : 200
+}
+
+let cards =[]//oh no its an array! Enough practise and ill be okay // array is empty at start of program
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
+let sumEl = document.getElementById("sum-el")//need to go over the logic again for these getElementById
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
 
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard(){
+    let randomNumber = Math.floor( Math.random()*13 ) + 1 
+    if (randomNumber > 10) { 
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }  
+}
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " // cards[0] + " " + cards[1]  I found the error!
+     for ( let i = 0; i < cards.length; i += 1) {
+        cardsEl.textContent += cards[i] + " "
+     }
+    
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -24,19 +54,11 @@ function startGame() {
 }
 
 function newCard(){
-    console.log("Drawing a new card from the deck!")
-    let card = 7
-    sum += card
-    startGame()
+    //console.log("Drawing a new card from the deck!")
+    if(isAlive === true && hasBlackJack === false){
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 }
-
-
-
-  /*if (sum > 21) optional */ 
-
-//  let age=22
-//   if (age<21){
-//     console.log("You can not enter this club!")
-// } else{
-//     console.log("Welcome!")
-// }
